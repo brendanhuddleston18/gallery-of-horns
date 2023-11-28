ce1d070f1895335d24e42835cef1ca4faa617aa7
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-27 20:05:55 -0600

    added titles to gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -34,8 +34,8 @@ function Gallery() {
 
 return (
   <>
-        <HornedBeasts name="Rhino" src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" description = "This is a horned beast" />
-        <HornedBeasts name='Unicorn' src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" description = "This is a horned beast" />
+        <HornedBeasts title="title" name="Rhino" src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" description = "This is a horned beast" />
+        <HornedBeasts title ="title" name='Unicorn' src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" description = "This is a horned beast" />
   </>
 
 )
