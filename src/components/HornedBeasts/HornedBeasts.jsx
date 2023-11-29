0f5cbaef7a0e42deb7200f1c499fd5d4063bee60
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-29 11:12:17 -0600

    removed comments from hornedbeasts

--- src/components/HornedBeasts/HornedBeasts.jsx
+++ src/components/HornedBeasts/HornedBeasts.jsx
@@ -36,16 +36,7 @@ function HornedBeasts(props) {
         </Card.Body>
       </Card>
     </Col>
-      )
-    
+      )  
     }
-      /* <div className={styles.hornedBeasts}>
-        <h2>{props.title}</h2>
-        <img src={props.src} alt={props.description} title={props.title}></img>
-        <p>{props.description}</p>
-        <p>Horns: {props.horns}</p>
-        <p><strong>This animal has {votes} votes!</strong></p>
-        <button style={{display: `${visible}`}}onClick={favorite}>{'\u2764'}</button>
-      </div> */
 
 export default HornedBeasts;
