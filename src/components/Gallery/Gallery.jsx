056ff105204dae49efd53950823237c3e6c59207
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-29 22:24:16 -0600

    formatted

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import HornedBeasts from "../HornedBeasts/HornedBeasts.jsx";
 import SelectedBeasts from "../SelectedBeasts/SelectedBeasts.jsx";
 import styles from "./Gallery.module.css";
@@ -10,46 +10,45 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Gallery(props) {
-  
-  
   let beastArray = props.beasts.map((value) => {
     return value;
-    })
+  });
 
-    const [selectedBeasts, setBeasts] = useState(null);
+  const [selectedBeasts, setBeasts] = useState(null);
 
-    function selectBeasts(choice){
-      setBeasts(choice);
-      console.log(choice);
-    }
+  function selectBeasts(choice) {
+    setBeasts(choice);
+    console.log(choice);
+  }
 
   // let selectedBeast = selectBeasts()
-    // console.log(selectedBeast);
+  // console.log(selectedBeast);
   return (
     <div className={styles.Gallery}>
-        <Container className={styles.Container} fluid>
-          <Row className={styles.row}>
-            {props.beasts.map((beast) => (
-              <HornedBeasts
-                key={beast._id}
-                beast={beast}
-                // state={props.state}
-                selectBeasts={selectBeasts}
-                showModal={props.showModal}
-              />
-            ))}
-          </Row>
-        </Container>
-      <SelectedBeasts className={styles.Modal} 
-                // onClick={handleClick}
-                // state={props.state}
-                showModal={props.showModal} 
-                display={props.display} 
-                hideModal={props.hideModal} 
-                beastArray={beastArray}
-                selectedBeast={selectedBeasts}
-                />
-      </div>
+      <Container className={styles.Container} fluid>
+        <Row className={styles.row}>
+          {props.beasts.map((beast) => (
+            <HornedBeasts
+              key={beast._id}
+              beast={beast}
+              // state={props.state}
+              selectBeasts={selectBeasts}
+              showModal={props.showModal}
+            />
+          ))}
+        </Row>
+      </Container>
+      <SelectedBeasts
+        className={styles.Modal}
+        // onClick={handleClick}
+        // state={props.state}
+        showModal={props.showModal}
+        display={props.display}
+        hideModal={props.hideModal}
+        beastArray={beastArray}
+        selectedBeast={selectedBeasts}
+      />
+    </div>
   );
 }
 
